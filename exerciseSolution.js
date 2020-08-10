da632e9a70b4eee9619881db5881ce9e06b7e5c1
kartish-A <abd.kartish@gmail.com>
2020-08-10 14:45:53 +0200

    ExerciseSolution

---
+++ exerciseSolution.js
@@ -0,0 +1,26 @@
+// first question:
+let myString = "I can walk in the park all day!";
+let slicedString = myString.slice(17, 22);
+console.log(slicedString);
+
+
+// second question:
+let language = "JavaScript";
+let slicedLanguage = language.slice(3, 6);
+console.log(slicedLanguage);
+
+// third question:
+let opinion = "nice shoes";
+let ifSubOpinionIncluded = opinion.includes("l");
+console.log(ifSubOpinionIncluded);
+
+let ifSubOpinionIncluded2 = opinion.includes("n");
+console.log(ifSubOpinionIncluded2);
+
+
+// fourth question:
+let firstName = "Abdoulfatah";
+let myCity = "Berlin";
+let myExperience = "this is my first JavaScript course.";
+console.log("Hello there! "  +  "My name is: "  +  firstName  +  " and I am living in: "  +  myCity  +  " and about my experience in JavaScript: "  +  myExperience);
+
