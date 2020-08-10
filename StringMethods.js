1849c726e6ef14d79bc841a7ace1266df8a56d41
kinan Naem <kinan.berlin@gmail.com>
2020-08-10 14:43:43 +0200

    Create StringMethods.js
    Completed
    
    Co-Authored-By: Mariam Fatima <mariamfatima.co@gmail.com>
    Co-Authored-By: Ghassan Aldarwish <ghassanaldarwish@yahoo.com>

---
+++ StringMethods.js
@@ -0,0 +1,15 @@
+let firstSantance = "I can walk in the park all day!";
+console.log(firstSantance.slice(18,23)); //park
+
+let seconedSantance = "JavaScript";
+console.log(seconedSantance.slice(3,6)); //aSc
+
+let thirdSantance = "nice shoes";
+console.log(thirdSantance.includes("n"));//true
+
+let name="Kinan Naem";
+let city ="Berlin";
+let pro = "Graphic and Web Designer ";
+
+console.log(`My name is ${name}, I live curently in ${city} and I work as ${pro}`);
+
